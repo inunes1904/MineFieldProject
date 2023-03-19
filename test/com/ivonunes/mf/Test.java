019752a8a14fa32f7a3a00bd0e54ca5cf4cdaa83
Ivo Nunes <inunes1904@gmail.com>
2023-03-19 09:58:02 +0000

    test library configured

---
+++ test/com/ivonunes/mf/Test.java
@@ -0,0 +1,18 @@
+package com.ivonunes.mf;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    void test(){
+        int a = 1+1;
+        assertEquals(2,a);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testIfequalsThree(){
+        int x = 2+10-9;
+        assertEquals(3, x);
+    }
+
+}
